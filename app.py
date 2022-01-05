031bfb7d52fdb885e84b86f79b2cbdeb8e231404
Christian Thompson <ChristianThompson1@hotmail.com>
2022-01-04 20:39:58 -0800

    working on adding the ability to upload photos

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import os
 import datetime
 
 app = Flask(__name__)
 host = os.environ.get('DB_URL')
+mongo= PyMongo(app)
+client = MongoClient(host=host)
+
+db = client.virtcloset
+
+item = db.clothingitem
+collection = db.collection
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    if 'clothing_image' in request.files:
+        clothing_image = request.files['clothing_image']
+        mongo.save_file(clothing_image.filename)
+        mongo.db.users.insert({'clothing_type' : request.form.get('clothing_type'), 'clothing_image_name' : clothing_image.filename})
+
+        return 'Done!'
 
 
 if __name__ == "__main__":
