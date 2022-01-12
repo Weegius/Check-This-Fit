ba1a351d397608b18906b727ff21fe7cb4d22e67
Christian Thompson <ChristianThompson1@hotmail.com>
2022-01-12 01:44:41 -0800

    added many features

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bson.objectid import ObjectId
 from flask_bcrypt import Bcrypt
 import datetime
 import os
+import requests
 
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ clothes = db.clothes
 users = db.user
 
 
+''' SHOW ALL Clothing Item------------------------------------------------- '''
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -30,28 +32,69 @@ def home():
 
 
 
+''' CREATE NEW Clothing Item ------------------------------------------------- '''
+@app.route("/clothes/new", methods=['GET'])
+def clothes_new():
+    _id = session['_id']
+    return render_template('clothing_new.html', title='New Clothing Item', _id=_id)
+
+
+
+
+''' SUBMIT A NEW Clothing Item ------------------------------------------------- '''
+@app.route("/clothes", methods=['POST'])
+def clothes_submit():
+    _id = session['_id']
+    clothe = {
+        'name': request.form.get('title'),
+        'type': request.form.get('type'),
+        'created': datetime.datetime.utcnow(),
+        'user_id': _id
+    }
+    clothes.insert_one(clothe)
+    return redirect(url_for('user'))
+
+
+
+
+''' EDIT A Clothing Item  ---------------------------------------------------------'''
+@app.route("/clothes/<clothes_id>/edit")
+def clothes_edit(clothes_id):
+    clothe = clothes.find_one({'_id': ObjectId(clothes_id)})
+    return render_template('clothes_edit.html', clothe = clothe)
 
 
 
 
 
+''' SUBMIT THE EDITED Clothing Item ------------------------------------------------- '''
+@app.route("/clothes/<clothes_id>", methods=['POST'])
+def clothes_update(clothes_id):
+    updated_clothe = {
+        'name': request.form.get('title'),
+        'content': request.form.get('description'),
+        'created': datetime.datetime.utcnow(),
+    }
+
+    clothes.update_one(
+        {'_id': ObjectId(clothes_id)},
+        {'$set': updated_clothe})
+    return redirect(url_for('user', clothess_id=clothes_id, title='Edit Clothing Item'))
 
 
 
 
+''' DELETE A Clothing Item ------------------------------------------------- '''
+
+@app.route("/clothes/<clothes_id>/delete", methods=['POST'])
+def clothess_delete(clothes_id):
+    """Delete one playlist."""
+    clothes.delete_one({'_id': ObjectId(clothes_id)})
+    return redirect(url_for('user'))
 
 
 
 
-
-
-
-
-
-
-
-
-    
 
 
 '''HELPER FUNCTIONS ---------------------------------------------------------------------- '''
@@ -68,19 +111,24 @@ def current_user():
     
 
 
+
+
 @app.route("/user")
 def user():
     if logged_in:
         user = current_user()
         _id = session['_id']
 
-        return render_template("user.html", _id=_id, user=user, )
+        return render_template("user.html", _id=_id, user=user, clothes=clothes.find({'user_id': _id}).sort([['_id', -1]]))
     else:
         flash("You are not logged in!")
         return redirect(url_for("login"))
         
 
-# notes=notes.find({'user_id': _id}).sort([['_id', -1]])
+
+
+
+
 '''LOGIN AND LOGOUT ROUTES -----------------------------------------'''
 
 
